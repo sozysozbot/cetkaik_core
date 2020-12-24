f0ecb63c18abed77a20b74697ac4083a53e249aa
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-12-25 02:33:29 +0900

    implement to_relative stuffs

--- src/perspective.rs
+++ src/perspective.rs
@@ -27,6 +27,30 @@ pub fn to_absolute_board(board: &relative::Board, p: Perspective) -> absolute::B
 }
 
 #[must_use]
+pub fn to_relative_board(board: &absolute::Board, p: Perspective) -> relative::Board {
+    let mut ans = [
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None, None],
+    ];
+
+    for i in 0..8 {
+        for j in 0..8 {
+            if let Some(piece) = board.get(&to_absolute_coord([i, j], p)) {
+                ans[i][j] = Some(to_relative_piece(*piece, p))
+            }
+        }
+    }
+    ans
+}
+
+#[must_use]
 pub fn to_absolute_field(field: relative::Field, p: Perspective) -> absolute::Field {
     let relative::Field {
         hop1zuo1of_downward,
@@ -36,28 +60,6 @@ pub fn to_absolute_field(field: relative::Field, p: Perspective) -> absolute::Fi
     absolute::Field {
         board: to_absolute_board(&current_board, p),
         ia_side_hop1zuo1: match p {
-            Perspective::IaIsDownAndPointsUpward => hop1zuo1of_downward
-                .iter()
-                .copied()
-                .map(
-                    |relative::NonTam2PieceDownward { color, prof }| absolute::NonTam2Piece {
-                        color,
-                        prof,
-                    },
-                )
-                .collect(),
-            Perspective::IaIsUpAndPointsDownward => hop1zuo1of_upward
-                .iter()
-                .copied()
-                .map(
-                    |relative::NonTam2PieceUpward { color, prof }| absolute::NonTam2Piece {
-                        color,
-                        prof,
-                    },
-                )
-                .collect(),
-        },
-        a_side_hop1zuo1: match p {
             Perspective::IaIsDownAndPointsUpward => hop1zuo1of_upward
                 .iter()
                 .copied()
@@ -79,6 +81,55 @@ pub fn to_absolute_field(field: relative::Field, p: Perspective) -> absolute::Fi
                 )
                 .collect(),
         },
+        a_side_hop1zuo1: match p {
+            Perspective::IaIsDownAndPointsUpward => hop1zuo1of_downward
+                .iter()
+                .copied()
+                .map(
+                    |relative::NonTam2PieceDownward { color, prof }| absolute::NonTam2Piece {
+                        color,
+                        prof,
+                    },
+                )
+                .collect(),
+            Perspective::IaIsUpAndPointsDownward => hop1zuo1of_upward
+                .iter()
+                .copied()
+                .map(
+                    |relative::NonTam2PieceUpward { color, prof }| absolute::NonTam2Piece {
+                        color,
+                        prof,
+                    },
+                )
+                .collect(),
+        },
+    }
+}
+
+#[must_use]
+pub fn to_relative_field(field: absolute::Field, p: Perspective) -> relative::Field {
+    let absolute::Field {
+        board,
+        ia_side_hop1zuo1,
+        a_side_hop1zuo1,
+    } = field;
+
+    relative::Field {
+        hop1zuo1of_downward: match p {
+            Perspective::IaIsUpAndPointsDownward => ia_side_hop1zuo1.iter().copied(),
+            Perspective::IaIsDownAndPointsUpward => a_side_hop1zuo1.iter().copied(),
+        }
+        .map(
+            |absolute::NonTam2Piece { color, prof }| relative::NonTam2PieceDownward { color, prof },
+        )
+        .collect(),
+        hop1zuo1of_upward: match p {
+            Perspective::IaIsUpAndPointsDownward => a_side_hop1zuo1.iter().copied(),
+            Perspective::IaIsDownAndPointsUpward => ia_side_hop1zuo1.iter().copied(),
+        }
+        .map(|absolute::NonTam2Piece { color, prof }| relative::NonTam2PieceUpward { color, prof })
+        .collect(),
+        current_board: to_relative_board(&board, p),
     }
 }
 
@@ -101,6 +152,18 @@ pub const fn to_relative_side(side: absolute::Side, p: Perspective) -> relative:
         | (absolute::Side::ASide, Perspective::IaIsUpAndPointsDownward) => relative::Side::Upward,
         (absolute::Side::IASide, Perspective::IaIsUpAndPointsDownward)
         | (absolute::Side::ASide, Perspective::IaIsDownAndPointsUpward) => relative::Side::Downward,
+    }
+}
+
+#[must_use]
+pub const fn to_relative_piece(piece: absolute::Piece, p: Perspective) -> relative::Piece {
+    match piece {
+        absolute::Piece::Tam2 => relative::Piece::Tam2,
+        absolute::Piece::NonTam2Piece { prof, color, side } => relative::Piece::NonTam2Piece {
+            prof,
+            color,
+            side: to_relative_side(side, p),
+        },
     }
 }
 
@@ -158,4 +221,39 @@ pub fn to_absolute_coord(coord: relative::Coord, p: Perspective) -> absolute::Co
         rows[if p.ia_is_down() { row } else { 8 - row }],
         columns[if p.ia_is_down() { col } else { 8 - col }],
     )
+}
+
+#[must_use]
+pub fn to_relative_coord(coord: absolute::Coord, p: Perspective) -> relative::Coord {
+    let (row, col) = coord;
+
+    let columns_col = match col {
+        absolute::Column::K => 0,
+        absolute::Column::L => 1,
+        absolute::Column::N => 2,
+        absolute::Column::T => 3,
+        absolute::Column::Z => 4,
+        absolute::Column::X => 5,
+        absolute::Column::C => 6,
+        absolute::Column::M => 7,
+        absolute::Column::P => 8,
+    };
+
+    let rows_row = match row {
+        absolute::Row::A => 0,
+        absolute::Row::E => 1,
+        absolute::Row::I => 2,
+        absolute::Row::U => 3,
+        absolute::Row::O => 4,
+        absolute::Row::Y => 5,
+        absolute::Row::AI => 6,
+        absolute::Row::AU => 7,
+        absolute::Row::IA => 8,
+    };
+
+    if p.ia_is_down() {
+        [rows_row, columns_col]
+    } else {
+        [8 - rows_row, 8 - columns_col]
+    }
 }
